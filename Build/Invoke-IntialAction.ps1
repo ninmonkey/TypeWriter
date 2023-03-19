15eb4ba5a96bb85cb0b476480a6b9244f059c9eb
Jake Bolton <ninmonkeys@gmail.com>
2023-03-19 11:02:39 -0500

    actions boilerplate

---
+++ Build/Invoke-IntialAction.ps1
@@ -0,0 +1,21 @@
+Install-Module PSDevOps -Scope CurrentUser -Force
+Import-Module PSDevOps -Force -PassThru | Out-Host
+
+# this builds 'TypeWriter\.github\workflows\TestAndPublish.yml'
+mkdir .\github | Out-Null
+mkidr .\github\workflows | Out-Null
+
+$newGitHubWorkflowSplat = @{
+    Name = "Analyze, Test, Tag, and Publish"
+    On = 'Push', 'PullRequest', 'Demand'
+    Job = 'PowerShellStaticAnalysis', 'TestPowerShellOnLinux', 'TagReleaseAndPublish'
+}
+$YamlFilename = 'github/workflows/TestAndPublish.yml'
+$setContentSplat = @{
+    Encoding = 'UTF8'
+    PassThru = $true
+    Path = Join-Path $PSScriptRoot $YamlFilename
+}
+New-Item -Path (Join-Path $PSScriptRoot $YamlFilename) -itemType File -Force -ea 'SilentlyContinue' | Out-Null
+New-GitHubWorkflow @newGitHubWorkflowSplat
+| Set-Content @setContentSplat -PassThru -Force
