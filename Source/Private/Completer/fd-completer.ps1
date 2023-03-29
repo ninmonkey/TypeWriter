802edc23c5b87a82ba046f4865300746b361060a
Jake Bolton <ninmonkeys@gmail.com>
2023-03-29 10:38:44 -0500

    Create fd-completer.ps1

---
+++ Source/Private/Completer/fd-completer.ps1
@@ -0,0 +1,28 @@
+# New-completion?
+# function completers.fdFind {}
+# function completions.fdFind {}
+
+function Register-TypeCompleterCommandFdFind {
+    <#
+    .SYNOPSIS
+    wrap completions for Fd Search
+
+    .DESCRIPTION
+    from <https://github.com/sharkdp/fd#features>
+
+    .EXAMPLE
+    An example
+
+    .NOTES
+    General notes
+    .LINK
+        https://github.com/sharkdp/fd#features
+    .LINK
+        https://docs.rs/regex/1.0.0/regex/#syntax
+    #>
+
+}
+
+function Register-TypeCompleterCommandFdFind {
+
+}
